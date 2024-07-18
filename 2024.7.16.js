0cb6f7d219448fc5ae034545626d5f016e2485df
misakisaysyes <misakisaysyes@gmail.com>
2024-07-18 15:24:25 +0800

    feat: 2024.7.18

--- 2024.7.16.js
+++ 2024.7.16.js
@@ -16,3 +16,44 @@ const findSum = (nums, target) => {
 }
 
 console.log(findSum([10,  30,  26,  31,  47,  60,  18,  48,  11,  67], 78));
+
+
+// 实现一个串行异步任务队列
+class Queue {
+    constructor(asyncCb) {
+        this.asyncCb = asyncCb;
+        this.lock = false;
+        this.prevPromise = [];
+    }
+
+    // 请求竞争
+    raceCall(...param) {
+        if(this.lock) {
+            setTimeout(() => {
+                this.call(...param);
+            }, 500);
+        } 
+        this.lock = true;
+        return this.asyncCb(...param).finally(() => {
+            console.log(param);
+            this.lock = false 
+        });
+    }
+
+    // 串行请求
+    syncCall(...param) {
+        let p = this.asyncCb(...param);
+        this.prevPromise.push(p);
+        return Promise.allSettled(this.prevPromise).then(() => p);
+    }
+}
+
+const queue = new Queue(async (param) => {
+    await new Promise(resolve => setTimeout(resolve, 1000 * Math.random()));
+    return param + 2;
+});
+ 
+queue.syncCall(1).then(console.log);
+queue.syncCall(2).then(console.log);
+queue.syncCall(3).then(console.log);
+
